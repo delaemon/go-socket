b2f13d0ff389e15e7ba9c7a643e7c4c4778cfdd6
delaemon <takeshi.music.5@gmail.com>
2015-08-18 23:29:52 +0900

    use goroutine

--- src/server.go
+++ src/server.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"time"
 )
 
 func main() {
-	service := ":7777"
+	service := ":1200"
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
 	checkErrorServer(err)
 	listener, err := net.ListenTCP("tcp", tcpAddr)
@@ -18,9 +19,33 @@ func main() {
 		if err != nil {
 			continue
 		}
-		daytime := time.Now().String()
-		conn.Write([]byte(daytime))
-		conn.Close()
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	conn.SetReadDeadline(time.Now().Add(2 * time.Minute))
+	request := make([]byte, 128)
+	defer conn.Close()
+	for {
+		read_len, err := conn.Read(request)
+		fmt.Println(read_len)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		if read_len == 0 {
+			break
+		} else if string(request) == "timestamp" {
+			daytime := strconv.FormatInt(time.Now().Unix(), 10)
+			conn.Write([]byte(daytime))
+		} else {
+			daytime := time.Now().String()
+			conn.Write([]byte(daytime))
+		}
+
+		request = make([]byte, 128)
 	}
 }
 
